d95e56ce842cce6ee9069fed2af44193cac35d98
ajewe <ajewe@utexas.edu>
2019-12-15 09:48:56 -0600

    added info on the pokemon pulled

--- pokemon.js
+++ pokemon.js
@@ -1,37 +1,55 @@
-let pokeObj = {};
+let pokeObj1 = {};
+let pokeObj2 = {};
 
 window.onload = function() {
-  getPokemon();
+  getRandomPokemon1();
+  getRandomPokemon2();
 }
 
-const getPokemon = () => {
-  fetch('https://pokeapi.co/api/v2/pokemon/3/')
+function getRandomInt(max) {
+  console.log(Math.floor(Math.random() * Math.floor(max)));
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+const getRandomPokemon1 = () => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${getRandomInt(807)}/`)
     .then(res => res.json())
-    .then(pokemon => pokeObj = pokemon)
+    .then(pokemon => pokeObj1 = pokemon)
 }
 
-// eventually do math.random to pull in a random set of pokemon
+const getRandomPokemon2 = () => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${getRandomInt(807)}/`)
+  .then(res => res.json())
+  .then(pokemon => pokeObj2 = pokemon)
+}
 
-
-const showPokemon = () => {
-  console.log('clicked');
+const getPokemon1 = () => {
 
   const pokemon1 = document.getElementById('pokemon1')
+  const li = document.createElement('li')
+  const pic = document.createElement('img')
+  const text = document.createTextNode(`Name: ${pokeObj1.species.name}, Type: ${pokeObj1.types[0].type.name}, Weight: ${pokeObj1.weight}`)
+  
+  pic.src = pokeObj1.sprites.front_default
+  
+  li.appendChild(pic)
 
-  // pokeObj.sprites ((pokemon, index) => {
-    console.log(pokeObj);
+  pokemon1.append(li);
+  pokemon1.append(text);
+}
 
-    const li = document.createElement('li')
-    const pic = document.createElement('img')
-    
-    pic.src = pokeObj.sprites.front_default
-    // const text = document.createTextNode(`#${index}, First Name: ${user.name.first}, Last Name: ${user.name.last} `)
-    
-    li.appendChild(pic)
-    // li.appendChild(text)
-    
-    pokemon1.append(li)
 
-  // })
+const getPokemon2 = () => {
 
+  const pokemon1 = document.getElementById('pokemon1')
+  const li = document.createElement('li')
+  const pic = document.createElement('img')
+  const text = document.createTextNode(`Name: ${pokeObj1.species.name}, Type: ${pokeObj1.types[0].type.name}, Weight: ${pokeObj1.weight}`)
+  
+  pic.src = pokeObj1.sprites.front_default
+  
+  li.appendChild(pic)
+
+  pokemon1.append(li);
+  pokemon1.append(text);
 }
